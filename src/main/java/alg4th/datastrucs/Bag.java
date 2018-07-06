772c30eabca36b235f84bb295b7308489ab9926f
panguangze <guangzep@gmail.com>
2018-07-06 13:25:24 +0800

    添加测试文件

---
+++ src/main/java/alg4th/datastrucs/Bag.java
@@ -0,0 +1,10 @@
+package alg4th.datastrucs;
+
+/**
+ * Bag is a kind of datastruct that only supposed to add,no deleting operation
+ * @author  panguangze
+ */
+public class Bag {
+
+    
+}
---
+++ src/test/java/alg4th/datastrucs/TestStack.java
@@ -0,0 +1,29 @@
+package alg4th.datastrucs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestStack {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPush() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIterator() {
+		fail("Not yet implemented");
+	}
+
+}
