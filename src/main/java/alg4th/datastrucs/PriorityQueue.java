3cc84a6237b850951a20902453720cb01e6e7113
lucky-pan <guangzep@gmail.com>
2018-07-09 22:39:21 +0800

    添加优先队列

---
+++ src/main/java/alg4th/datastrucs/PriorityQueue.java
@@ -0,0 +1,10 @@
+package alg4th.datastrucs;
+
+/**
+ * PriorityQueue,The abstract super class of queues.<br>
+ * @author  panguangze
+ */
+public abstract class PriorityQueue {
+
+    public abstract void insert();
+}
