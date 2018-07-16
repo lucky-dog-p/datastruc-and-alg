0858a5a9dac860b47872a63801421d7faf53b439
panguangze <guangzep@gmail.com>
2018-07-16 14:52:34 +0800

    添加基于数组的PQ

---
+++ src/main/java/alg4th/datastrucs/ArrayMinPQ.java
@@ -0,0 +1,89 @@
+package alg4th.datastrucs;
+
+/**
+ * MinPQ,min priority queue
+ */
+public class ArrayMinPQ<Key extends Comparable<Key>> extends PriorityQueue<Key>{
+
+    private Key[] array;
+    private int N;
+
+    /**
+     * Create a minPQ of initial capacity N.
+     */
+    public ArrayMinPQ(int N){
+        array = (Key[]) new Comparable[N + 1];
+    }
+
+    /**
+     * Create a minPQ from the keys in array[].
+     */
+    public ArrayMinPQ(Key[] array){
+        this.array = (Key[]) new Comparable[array.length + 1];
+        for (int i = 0; i < array.length; i++) {
+            this.array[i+1] = array[i];
+        }
+        for(int i = this.array.length/2; i >=1;i--){
+            sink(i);
+        }
+    }
+
+    @Override
+    public void insert(Key k) {
+
+        array[++N] = k;
+        swim(N);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return N==0;
+    }
+
+    @Override
+    public int size() {
+        return N;
+    }
+
+    public Key min(){
+        return array[1];
+    }
+
+    public Key delMin(){
+        Key temp = array[1];
+        exchange(1,N--);
+        array[N+1] = null;
+        sink(1);
+        return temp;
+    }
+
+    private void swim(int i){
+        while(i > 1 && less(i, i/2)){
+            exchange(i, i/2);
+            i = i/2;
+        }
+    }
+
+    private void sink(int i){
+        while(2*i <= N){
+            int j = 2*i;
+            //choose the smaller child node
+            if(j<N && !less(j, j+1)) j++;
+            //if current node is less than its children nodes,break
+            if(less(i, j)) break;
+            //else change the value and go on sinking.
+            exchange(i, j);
+            i = j;
+        }
+    }
+
+    private void exchange(int i,int j){
+        Key temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+    }
+
+    private boolean less(int i,int j){
+        return array[i].compareTo(array[j]) < 0;
+    }
+}
